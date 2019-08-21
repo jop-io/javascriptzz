b140894758cf87e7810dc9f79a63053643248574
Jonas Persson <11533019+jop-io@users.noreply.github.com>
2019-08-21 13:47:12 +0200

    Update validateSuggestEmail.js

--- validateSuggestEmail.js
+++ validateSuggestEmail.js
@@ -1,36 +1,36 @@
 function validateSuggestEmail(email)
 {
-	let match = email
-  		.toString()
+  let match = email
+      .toString()
       .toLowerCase()
       .match(/^([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)@([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)\.([a-zA-Z0-9]{2,})$/);
-	
-	if (!match) {
-		return false;
-	}
   
-	let user = match[1],
-			host = match[3],
-			tld  = match[5];
+  if (!match) {
+    return false;
+  }
   
-	let commons = [
-		{ "host": "gmail",     "tlds": ["com"]       },
-		{ "host": "yahoo",     "tlds": ["com", "se"] },
-		{ "host": "hotmail",   "tlds": ["com", "se"] },
-		{ "host": "telia",     "tlds": ["com"]       },
-		{ "host": "icloud",    "tlds": ["com"]       },
-		{ "host": "live",      "tlds": ["com", "se"] },
-		{ "host": "outlook",   "tlds": ["com"]       },
-		{ "host": "tele2",     "tlds": ["com"]       },
-		{ "host": "bahnhof",   "tlds": ["se"]        },
-		{ "host": "bredband",  "tlds": ["net"]       },
-		{ "host": "bredband2", "tlds": ["com"]       },
-		{ "host": "comhem",    "tlds": ["se"]        },
-		{ "host": "home",      "tlds": ["se"]        },
-		{ "host": "me",        "tlds": ["com"]       },
-		{ "host": "msn",       "tlds": ["com"]       },
-		{ "host": "spray",     "tlds": ["se"]        },
-	];
+  let user = match[1],
+      host = match[3],
+      tld  = match[5];
+  
+  let commons = [
+    { "host": "gmail",     "tlds": ["com"]       },
+    { "host": "yahoo",     "tlds": ["com", "se"] },
+    { "host": "hotmail",   "tlds": ["com", "se"] },
+    { "host": "telia",     "tlds": ["com"]       },
+    { "host": "icloud",    "tlds": ["com"]       },
+    { "host": "live",      "tlds": ["com", "se"] },
+    { "host": "outlook",   "tlds": ["com"]       },
+    { "host": "tele2",     "tlds": ["com"]       },
+    { "host": "bahnhof",   "tlds": ["se"]        },
+    { "host": "bredband",  "tlds": ["net"]       },
+    { "host": "bredband2", "tlds": ["com"]       },
+    { "host": "comhem",    "tlds": ["se"]        },
+    { "host": "home",      "tlds": ["se"]        },
+    { "host": "me",        "tlds": ["com"]       },
+    { "host": "msn",       "tlds": ["com"]       },
+    { "host": "spray",     "tlds": ["se"]        },
+  ];
   
   let ld = function(s, t) {
     if (s === t) {
@@ -83,26 +83,26 @@ function validateSuggestEmail(email)
     }
     return h;
   };
-	
-	for (let i in commons)
+  
+  for (let i in commons)
   {
-  	let c = commons[i];
+    let c = commons[i];
     
     // hostname is correct, TLD is correct
     if (host === c.host && c.tlds.indexOf(tld) >= 0) {
-    	return true;
+      return true;
     }
     
     // hostname is correct, but TLD is wrong
-  	if (host === c.host && c.tlds.indexOf(tld) < 0) {
-    	return user + "@" + host + "." + c.tlds[0];
+    if (host === c.host && c.tlds.indexOf(tld) < 0) {
+      return user + "@" + host + "." + c.tlds[0];
     }
     
     // levenstien comparsion on hostname
     if (ld(host, c.host) < 3) {
-    	return user + "@" + c.host + "." + (c.tlds.indexOf(tld) >= 0 ? tld : c.tlds[0]);
+      return user + "@" + c.host + "." + (c.tlds.indexOf(tld) >= 0 ? tld : c.tlds[0]);
     }
   }
-	
-	return true;
+  
+  return true;
 }
